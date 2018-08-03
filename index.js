b82af7e94cdbcbf88a733699c4003aaf37ec133b
Miguel Camba <miguel.camba@gmail.com>
2018-08-03 16:04:18 +0200

    v0.0.4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'ember-power-calendar-moment',
   included(app) {
     this._super.included(app);
-    app.import('vendor/ember-power-calendar-utils');
+    app.import('vendor/ember-power-calendar-utils/index.js');
   },
 
   treeForVendor(rawVendorTree) {
