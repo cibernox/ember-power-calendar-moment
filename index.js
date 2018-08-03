4dcbef83e28ac6c89f28140df321bbbe83d43be1
Miguel Camba <miguel.camba@gmail.com>
2018-08-03 15:39:48 +0200

    Export stuff from vendor

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-power-calendar-moment'
+  name: 'ember-power-calendar-moment',
+  treeForVendor(rawVendorTree) {
+    let babelAddon = this.addons.find(addon => addon.name === 'ember-cli-babel');
+
+    return babelAddon.transpileTree(rawVendorTree, {
+      'ember-cli-babel': {
+        compileModules: false
+      }
+    });
+  }
 };
