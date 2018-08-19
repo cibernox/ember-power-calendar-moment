ff40b4e1d760c8ca3aec019a18d8eff5689672c3
Miguel Camba <miguel.camba@gmail.com>
2018-08-19 15:33:21 +0200

    v0.0.11

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 module.exports = {
   name: 'ember-power-calendar-moment',
-  included(app) {
-    this._super.included(app);
-    app.import('vendor/ember-power-calendar-utils/index.js');
-  },
+  // included(app) {
+  //   this._super.included(app);
+  //   app.import('vendor/ember-power-calendar-utils/index.js');
+  // },
 
-  treeForVendor(rawVendorTree) {
-    let babelAddon = this.addons.find(addon => addon.name === 'ember-cli-babel');
+  // treeForVendor(rawVendorTree) {
+  //   let babelAddon = this.addons.find(addon => addon.name === 'ember-cli-babel');
 
-    return babelAddon.transpileTree(rawVendorTree, {
-      'ember-cli-babel': {
-        compileModules: true
-      }
-    });
-  }
+  //   return babelAddon.transpileTree(rawVendorTree, {
+  //     'ember-cli-babel': {
+  //       compileModules: true
+  //     }
+  //   });
+  // }
 };
